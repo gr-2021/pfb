6c860aedf8f73108c63487002917cc4936d50326
Gabriel Richter <75986085+xGabrielR@users.noreply.github.com>
2022-03-27 09:35:32 -0300

    Update heroku.py

--- blog/settings/heroku.py
+++ blog/settings/heroku.py
@@ -3,11 +3,13 @@ from blog.settings.base import *
 
 env = environ.Env()
 
-DEBUG = env.bool("DEBUG", False)
+#DEBUG = env.bool("DEBUG", False)
+DEBUG = False
 
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = '5408u459ty5498th548p9h458h8th584ht5498uy954jy945hy549hy459h##%$%*&&(' #env("SECRET_KEY")
 
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+#ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     "default": env.db(),
